3b30af9a915bd949b69256e39b405d73db5aa46c
nairind <jr36investors@gmail.com>
2025-05-24 21:54:28 +0100

    Update excelFileComparison.js

--- utils/excelFileComparison.js
+++ utils/excelFileComparison.js
@@ -1,4 +1,25 @@
+// File: utils/excelFileComparison.js
+
 import * as XLSX from "xlsx";
+
+export const parseExcelFile = (file) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      try {
+        const data = new Uint8Array(event.target.result);
+        const workbook = XLSX.read(data, { type: "array" });
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const json = XLSX.utils.sheet_to_json(worksheet, { defval: "" });
+        resolve(json);
+      } catch (error) {
+        reject(error);
+      }
+    };
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
+};
 
 function isNumeric(val) {
   return !isNaN(parseFloat(val)) && isFinite(val);
@@ -18,36 +39,29 @@ function compareWithTolerance(val1, val2, tolerance, type) {
   return false;
 }
 
-export const parseExcelFile = (file) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
+export const compareExcelFiles = async (file1, file2, finalMappings = []) => {
+  const [data1, data2] = await Promise.all([
+    parseExcelFile(file1),
+    parseExcelFile(file2),
+  ]);
 
-    reader.onload = (event) => {
-      try {
-        const data = new Uint8Array(event.target.result);
-        const workbook = XLSX.read(data, { type: "array" });
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        const json = XLSX.utils.sheet_to_json(worksheet, { defval: "" });
-        resolve(json);
-      } catch (error) {
-        reject(error);
+  const remappedData2 = data2.map(row => {
+    const newRow = {};
+    finalMappings.forEach(mapping => {
+      if (mapping.file1Header && mapping.file2Header) {
+        newRow[mapping.file1Header] = row[mapping.file2Header] ?? '';
       }
-    };
-
-    reader.onerror = reject;
-    reader.readAsArrayBuffer(file);
+    });
+    return newRow;
   });
-};
 
-const compareExcelData = (data1, data2, finalMappings = []) => {
   const results = [];
-  const maxRows = Math.max(data1.length, data2.length);
-  let matches = 0;
-  let differences = 0;
+  let matches = 0, differences = 0;
+  const maxRows = Math.max(data1.length, remappedData2.length);
 
   for (let i = 0; i < maxRows; i++) {
     const row1 = data1[i] || {};
-    const row2 = data2[i] || {};
+    const row2 = remappedData2[i] || {};
     const keys = new Set([...Object.keys(row1), ...Object.keys(row2)]);
     const fieldResults = {};
 
@@ -75,17 +89,11 @@ const compareExcelData = (data1, data2, finalMappings = []) => {
         difference: isNumeric(val1) && isNumeric(val2) ? Math.abs(val1 - val2).toFixed(2) : ''
       };
 
-      if (status === 'match' || status === 'acceptable') {
-        matches++;
-      } else {
-        differences++;
-      }
+      if (status === 'match' || status === 'acceptable') matches++;
+      else differences++;
     }
 
-    results.push({
-      ID: row1['ID'] || i + 1,
-      fields: fieldResults
-    });
+    results.push({ ID: row1['ID'] || i + 1, fields: fieldResults });
   }
 
   return {
@@ -94,25 +102,4 @@ const compareExcelData = (data1, data2, finalMappings = []) => {
     matches_found: matches,
     results
   };
-};
-
-export const compareExcelFiles = async (file1, file2, finalMappings = []) => {
-  const [data1, data2] = await Promise.all([
-    parseExcelFile(file1),
-    parseExcelFile(file2)
-  ]);
-
-  let alignedData2 = data2;
-
-  if (finalMappings?.length) {
-    alignedData2 = data2.map(row => {
-      const remapped = {};
-      finalMappings.forEach(({ file1Header, file2Header }) => {
-        remapped[file1Header] = row[file2Header] ?? '';
-      });
-      return remapped;
-    });
-  }
-
-  return compareExcelData(data1, alignedData2, finalMappings);
 };
