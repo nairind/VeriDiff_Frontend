003a4669db8b2ea85f30de7db869bdec0f5dc439
nairind <jr36investors@gmail.com>
2025-05-29 03:50:51 +0100

    Create privacy.js

---
+++ pages/privacy.js
@@ -0,0 +1,9 @@
+export default function Privacy() {
+  return (
+    <div style={{ maxWidth: '800px', margin: '0 auto', padding: '2rem' }}>
+      <h1>Privacy Policy</h1>
+      <p>Last updated: [Date]</p>
+      // GDPR Articles 13 & 14 compliant content
+    </div>
+  );
+}
