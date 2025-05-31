6dc7ff8110fe6b43d467b1552f69ede9e655cf8e
nairind <jr36investors@gmail.com>
2025-05-31 21:20:42 +0100

    Create track.js

---
+++ pages/api/analytics/track.js
@@ -0,0 +1,9 @@
+/pages
+  /api
+    /usage
+      track.js      ← Replace this with the new version
+      current.js    ← Keep your existing file
+    /analytics      ← Create this new folder
+      track.js      ← Create this new file
+    /auth
+      [...nextauth].js ← Keep existing
