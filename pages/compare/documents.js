060b51385b9af344da2745f281aa561b1382b29a
nairind <jr36investors@gmail.com>
2025-06-03 14:52:15 +0100

    Update documents.js

--- pages/compare/documents.js
+++ pages/compare/documents.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Head from 'next.js';
+import Head from 'next/head';   
 import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
 import AuthGuard from '../../components/auth/AuthGuard';
