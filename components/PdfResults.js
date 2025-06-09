bce65532971cfea32e00f8abe9b37838ebba08ca
nairind <jr36investors@gmail.com>
2025-06-09 07:18:08 +0100

    Update PdfResults.js

--- components/PdfResults.js
+++ components/PdfResults.js
@@ -641,7 +641,12 @@ const PdfResults = ({ results, file1Name, file2Name, options = {} }) => {
         function highlightSearchText(text, searchTerm) {
             if (!searchTerm.trim()) return text;
             
+            const regex = new RegExp('(' + searchTerm.replace(/[.*+?^${}()|[\\\]]/g, '\\\\        function highlightSearchText(text, searchTerm) {
+            if (!searchTerm.trim()) return text;
+            
             const regex = new RegExp('(' + searchTerm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + ')', 'gi');
+            return text.replace(regex, '<span class="highlight">$1</span>');
+        }') + ')', 'gi');
             return text.replace(regex, '<span class="highlight">$1</span>');
         }
         
